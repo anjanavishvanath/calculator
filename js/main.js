804076667b90246cec83c186bccf72299c5c8341
“anjanavishvanath” <anjanavishvanath3@gmail.com>
2022-07-13 15:08:35 +0530

    CSS added, Class implimented

--- js/main.js
+++ js/main.js
@@ -1,57 +1,105 @@
-const numButtons = document.querySelectorAll('[data-nu]')
-const oprButtons = document.querySelectorAll('[data-op]')
-const eqlButton = document.querySelector('[data-eql]')
-const clrButton = document.querySelector('[data-all-clr]')
-const delButton = document.querySelector('[data-del]')
-
-const currDisplay = document.querySelector('[data-curr-op]')
-const prevDisplay = document.querySelector('[data-prev-op]')   
-
-let currNum = []
-let num1 = 0
-let num2 = 0
-let operand = ""
-
-numButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        if(operand == ""){
-            currNum.push(btn.innerHTML)
-            num1 = Number(currNum.reduce((a,b) => a+b))
-            currDisplay.innerHTML = num1
-        }else{
-            currNum.push(btn.innerHTML)
-            num2 = Number(currNum.reduce((a,b) => a+b))
-            currDisplay.innerHTML = `${num1} ${operand} ${num2}` 
-        }
-    })
-})
-
-oprButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        operand = btn.innerHTML
-        currNum = []
-        currDisplay.innerHTML = `${num1} ${operand}`
-    })
-})
-
-eqlButton.addEventListener('click', () => {
-    prevDisplay.innerHTML = `${num1} ${operand} ${num2}`
-    if(operand == '+'){
-        currDisplay.innerHTML = num1+num2
-    }else if(operand == '-'){
-        currDisplay.innerHTML = num1 - num2
-    }else if(operand == '*'){
-        currDisplay.innerHTML = num1 * num2
-    }else if(operand == '÷'){
-        currDisplay.innerHTML = num1 / num2
+class Claculator{
+    constructor(currD,prevD){
+        this.currD = currD
+        this.prevD = prevD
+        this.clear()
     }
+
+    clear(){
+        this.prevOperand = ""
+        this.currOperand = ""
+        this.operation = undefined
+        this.updateDisplay()
+    }
+
+    delete(){
+        this.currOperand = this.currOperand.slice(0,-1)
+    }
+
+    appendumber(number){
+        if(this.currOperand.includes(".") && number === ".") return
+        this.currOperand += number
+    }
+
+    chooseOperation(opr){
+        if(this.currOperand === "") return
+        if(this.prevOperand !== ""){
+            this.compute()
+        }
+        this.operation = opr
+        this.prevOperand = this.currOperand
+        this.currOperand = ""
+    }
+
+    compute(){
+        let total = 0
+        if(this.prevOperand === "" || this.currOperand === "") return
+        switch(this.operation){
+            case "+":
+                total = parseFloat(this.prevOperand)+parseFloat(this.currOperand)
+                break
+            case "-":
+                total = parseFloat(this.prevOperand)-parseFloat(this.currOperand)
+                break
+            case "*":
+                total = parseFloat(this.prevOperand)*parseFloat(this.currOperand)
+                break
+            case "÷":
+                total = parseFloat(this.prevOperand)/parseFloat(this.currOperand)
+                break
+            default:
+                break
+        }
+        this.currOperand = total.toString()
+        this.prevOperand = ""
+        this.operation = undefined
+    }
+
+    getDisplaytNumber(number){
+// impliment , n stuff
+    }
+
+    updateDisplay(){
+        //update the display with every call
+        this.currD.innerHTML = this.currOperand 
+        if(this.operation !== undefined){
+            this.prevD.innerHTML = `${this.prevOperand}${this.operation}`
+        }
+        
+    }
+}
+
+let btnNum = document.querySelectorAll("[data-num]")
+let btnOpr = document.querySelectorAll("[data-opr]")
+let btnAC = document.querySelector("[data-all-clr]")
+let btnDel = document.querySelector("[data-del]")
+let btnEql = document.querySelector("[data-eql]")
+
+let prevDisplayElem = document.querySelector("[data-prev-op]")
+let currDisplayELem = document.querySelector("[data-curr-op]")
+
+
+let Calc = new Claculator(currDisplayELem,prevDisplayElem) 
+
+
+btnNum.forEach(btn => btn.addEventListener('click',()=>{
+    Calc.appendumber(btn.innerHTML)
+    Calc.updateDisplay()
+}))
+
+btnOpr.forEach(btn => btn.addEventListener('click',()=>{
+    Calc.chooseOperation(btn.innerHTML)
+    Calc.updateDisplay()
+}))
+
+btnAC.addEventListener('click',()=>Calc.clear())
+
+btnEql.addEventListener('click',()=>{
+    Calc.compute()
+    Calc.updateDisplay()
 })
 
-clrButton.addEventListener('click', () => {
-    currNum = []
-    num1 = 0
-    num2 = 0
-    operand = ""
-    currDisplay.innerHTML = num1
-    prevDisplay.innerHTML = ""
+btnDel.addEventListener('click',()=>{
+    Calc.delete()
+    Calc.updateDisplay()
 })
